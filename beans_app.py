86b1addb271917495654a6503bbb7f0152f8ac63
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-12-06 13:32:16 +0100

    Update beans_app.py

--- beans_app.py
+++ beans_app.py
@@ -3,77 +3,113 @@ import io
 import pickle
 import numpy as np
 import cv2
-import requests
-import tensorflow as tf
+from tensorflow.keras.models import load_model
 import base64
 
-# GitHub raw file URL for the model
+
+# Load the model
 model_url = 'https://github.com/Exwhybaba/Bean_disease_classifier/raw/main/Imagemodel.hdf5'
 encoder_url = 'https://raw.githubusercontent.com/Exwhybaba/Beans_disease_classifier/main/encoder.sav'
 
-# Function to load the model from URL and save it locally
-def load_model_from_url(url, local_path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Save the model file locally
-        with open(local_path, 'wb') as f:
-            f.write(response.content)
+loaded_model = load_model(model_path)
 
-        # Load the model from the local file
-        loaded_model = tf.keras.models.load_model(local_path)
-        return loaded_model
-    else:
-        st.error(f"Failed to download the model file. Status code: {response.status_code}")
-        return None
+# Load the encoder
+with open(encoder_path, 'rb') as file:
+    encoder = pickle.load(file)
+    
+    
+# Descriptions for different predictions
+descriptions = {
+    'angular_leaf_spot': "Angular lesions with water-soaked margins, often with a yellow halo.",
+    'bean_rust': "Orange to rusty-brown powdery pustules on the undersides of leaves.",
+    'healthy': "Vibrant green leaves without lesions or rust-like growth."
+}
 
-# Function to load the encoder from URL
-def load_encoder_from_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        # Load the encoder from the binary stream
-        encoder_binary = io.BytesIO(response.content)
-        encoder = pickle.load(encoder_binary)
-        return encoder
-    else:
-        st.error(f"Failed to download the encoder file. Status code: {response.status_code}")
-        return None
+def classifier(image):
+    resize = cv2.resize(image, (150, 150))
+    rescaling = resize / 255.0
+    rescaling = rescaling.reshape((1, 150, 150, 3))
+    predictor = loaded_model.predict(rescaling, verbose=0)
+    predicted_class = np.argmax(predictor, axis=1)
+    predict = encoder.inverse_transform(predicted_class)
+    confidence = np.max(predictor) * 100  # Confidence as a percentage
+    return predict[0], confidence
 
-# Function to classify an image
-def classifier(image, loaded_model, encoder):
-    # Resize and preprocess the image
-    resized_image = cv2.resize(image, (150, 150))
-    preprocessed_image = resized_image / 255.0
-    preprocessed_image = preprocessed_image.reshape((1, 150, 150, 3))
+def encode_image_as_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read()).decode()
+    return encoded_image
 
-    # Make prediction using the loaded model
-    predictions = loaded_model.predict(preprocessed_image, verbose=0)
-    predicted_class = np.argmax(predictions, axis=1)
-    predicted_label = encoder.inverse_transform(predicted_class)[0]
-    confidence = np.max(predictions) * 100
+def main():
+    # Set page configuration
+    st.set_page_config(page_title="Bean Disease Detector", page_icon=":seedling:", layout="wide")
 
-    return predicted_label, confidence
 
-# Local path to save the model file
-local_model_path = 'Imagemodel.hdf5'
 
-# Load the model from the URL and save it locally
-loaded_model = load_model_from_url(model_url, local_model_path)
-encoder = load_encoder_from_url(encoder_url)
+    # Background image using custom CSS
+    st.markdown(
+        f"""
+        <style>
+            .stApp {{
+                background-image: url("data:image/jpeg;base64,{encode_image_as_base64('C:/Users/Administrator/Documents/Sandbox/pytouch/Bean/images/cowpea2.jpg')}");  
+                background-size: cover;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 
-# Check if loading was successful
-if loaded_model is not None and encoder is not None:
-    # Your Streamlit app code...
+    # App title and description
     st.title("Bean Disease Detector")
     st.markdown("## Upload an image.")
 
     # File uploader
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    # Example usage in main function
+    # Display image and classification
     if uploaded_file is not None:
+        # Decode the uploaded image
         image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
-        prediction, confidence = classifier(image, loaded_model, encoder)
-        st.success(f"Detection: {prediction} (Accuracy: {confidence:.2f}%)")
 
-        # Example: Display image
-        st.image(image, caption="Uploaded Image.", use_column_width=True)
+        # Convert BGR to RGB
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Display image and classification
+        col1, col2 = st.columns([3, 1]) 
+        with col1:
+            
+            st.image(image_rgb, caption="Uploaded Image.", use_column_width=True, width = 100)
+           
+        with col2:
+            st.markdown(
+                f"""
+                <style>
+                    .stApp {{
+                        background-image: url("data:image/jpeg;base64,{encode_image_as_base64('C:/Users/Administrator/Documents/Sandbox/pytouch/Bean/images/mossgreen.jpg')}");  
+                        background-size: cover;
+                    }}
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
+            
+            with st.spinner("Detecting..."):
+                prediction, confidence = classifier(image)
+            st.success(f"Detection: {prediction} (Accuracy: {confidence:.2f}%)")
+            
+            # Display dynamic description based on prediction
+            if prediction in descriptions:
+                st.markdown(f"### Description:\n{descriptions[prediction]}")
+
+
+    # Copyright notice
+    st.markdown(
+        """
+        ---
+        © 2023 Oyelayo Seye. All rights reserved.
+        """,
+        unsafe_allow_html=True,
+    )
+
+if __name__ == "__main__":
+    main()
