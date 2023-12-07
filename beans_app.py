b27ca78c2682c2aed9040dfe7339ab445b614b60
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-12-08 00:14:16 +0100

    Update beans_app.py

--- beans_app.py
+++ beans_app.py
@@ -57,9 +57,10 @@ def main():
         unsafe_allow_html=True,
     )
 
-    # App title and description with blue color
-    st.markdown("<h1 style='color: white;'>Bean Disease Detector</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='color: white;'>Upload an image.</h2>", unsafe_allow_html=True)
+    
+    # App title and description with white color and adjusted margin
+    st.markdown("<h1 style='color: white; margin-top: -30px;'>Bean Disease Detector</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='color: white; margin-top: -15px;'>Upload an image.</h2>", unsafe_allow_html=True)
 
 
 
