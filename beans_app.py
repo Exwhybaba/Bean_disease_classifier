473218d9178378c1dfa8b8dc9ac38c9410260096
Oyelayo Seye <seyeoyelayo@gmail.com>
2023-12-08 00:53:21 +0100

    Update beans_app.py

--- beans_app.py
+++ beans_app.py
@@ -8,15 +8,20 @@ import tensorflow as tf
 import base64
 from tensorflow.keras.models import load_model
 
-# Function to load the model
-def load_model_and_encoder():
-    loaded_model = load_model('Imagemodel.h5', compile=False)
-    with open('encoder.sav', 'rb') as f:
-        encoder = pickle.load(f)
-    return loaded_model, encoder
+loaded_model = load_model('Imagemodel.h5', compile=False)
 
-# Function to classify the image
-def classifier(image, loaded_model, encoder):
+# Load the encoder
+with open('encoder.sav', 'rb') as f:
+    encoder = pickle.load(f)
+
+# Descriptions for different predictions
+descriptions = {
+    'angular_leaf_spot': "Angular lesions with water-soaked margins, often with a yellow halo.",
+    'bean_rust': "Orange to rusty-brown powdery pustules on the undersides of leaves.",
+    'healthy': "Vibrant green leaves without lesions or rust-like growth."
+}
+
+def classifier(image):
     resize = cv2.resize(image, (150, 150))
     rescaling = resize / 255.0
     rescaling = rescaling.reshape((1, 150, 150, 3))
@@ -26,66 +31,57 @@ def classifier(image, loaded_model, encoder):
     confidence = np.max(predictor) * 100  # Confidence as a percentage
     return predict[0], confidence
 
-# Function to encode image as base64
 def encode_image_as_base64(image_path):
     with open(image_path, "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode()
     return encoded_image
 
-# Function to apply custom styles based on the background image
-def apply_custom_styles(background_image):
-    if background_image == "mossgreen":
-        title_color = "black"
-    else:
-        title_color = "white"
-
-    st.markdown(
-        f"""
-        <style>
-            .custom-title {{
-                color: {title_color} !important;
-                margin-top: -45px;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-# Function to update session state
-def update_session_state():
-    if 'background_image' not in st.session_state:
-        st.session_state.background_image = "crop_health"
-
-# Function to main app logic
 def main():
-    # Load the model and encoder
-    loaded_model, encoder = load_model_and_encoder()
-
-    # Update session state
-    update_session_state()
-
     # Set page configuration
     st.set_page_config(page_title="Bean Disease Detector", page_icon=":seedling:", layout="wide")
 
-    # Background image using custom CSS
+    # Background color using custom CSS
     st.markdown(
-        f"""
+        """
         <style>
-            .stApp {{
-                background-image: url("data:image/jpeg;base64,{encode_image_as_base64('./images/crop_health.jpg')}");  
+            body {
+                background-color: #000; /* Set your desired background color */
                 background-size: cover;
-            }}
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+    # App title and description with blue color and adjusted margin for title
+    st.markdown("<h1 class='custom-title'>Bean Disease Detector</h1>", unsafe_allow_html=True)
+    st.markdown("<h2 style='color: white; margin-top: -40px;'>Upload an image.</h2>", unsafe_allow_html=True)
+
+    # Apply custom styles using CSS
+    st.markdown(
+        """
+        <style>
+            .custom-title {
+                color: white;
+                margin-top: -45px;
+            }
         </style>
         """,
         unsafe_allow_html=True
     )
 
-    # App title and description
-    st.markdown("<h1 class='custom-title'>Bean Disease Detector</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='color: white; margin-top: -40px;'>Upload an image.</h2>", unsafe_allow_html=True)
-
-    # Apply custom styles based on the background image
-    apply_custom_styles(st.session_state.background_image)
+    # Apply white color to the file uploader label text using CSS
+    st.markdown(
+        """
+        <style>
+            .css-qrbaxs {
+                color: white !important;
+                display: block;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
     # File uploader label text with white color
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], key="file_uploader")
@@ -99,7 +95,7 @@ def main():
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Display image and classification
-        col1, col2 = st.columns([3, 1]) 
+        col1, col2 = st.columns([3, 1])
         with col1:
             st.image(image_rgb, caption="Uploaded Image.", use_column_width=True, width=100)
 
@@ -107,27 +103,31 @@ def main():
             st.markdown(
                 f"""
                 <style>
-                    .stApp {{
-                        background-image: url("data:image/jpeg;base64,{encode_image_as_base64('./images/mossgreen.jpg')}");  
+                    body {{
+                        background-color: #000; /* Set your desired background color */
                         background-size: cover;
                     }}
                 </style>
                 """,
-                unsafe_allow_html=True
+                unsafe_allow_html=True,
             )
 
             with st.spinner("Detecting..."):
-                prediction, confidence = classifier(image, loaded_model, encoder)
+                prediction, confidence = classifier(image)
             st.success(f"Detection: {prediction} (Accuracy: {confidence:.2f}%)")
 
             # Display dynamic description based on prediction
-            descriptions = {
-                'angular_leaf_spot': "Angular lesions with water-soaked margins, often with a yellow halo.",
-                'bean_rust': "Orange to rusty-brown powdery pustules on the undersides of leaves.",
-                'healthy': "Vibrant green leaves without lesions or rust-like growth."
-            }
             if prediction in descriptions:
                 st.markdown(f"### Description:\n{descriptions[prediction]}")
+
+    # Copyright notice
+    st.markdown(
+        """
+        ---
+        © 2023 Oyelayo Seye. All rights reserved.
+        """,
+        unsafe_allow_html=True,
+    )
 
 if __name__ == "__main__":
     main()
